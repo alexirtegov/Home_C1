ce99af875ad107b8bbffa1728aef3422f88f759a
Alexey Irtegov <irteg@mail.ru>
2025-04-25 20:49:55 +0500

    hw1

---
+++ hw1/Let's.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //ПОДГРУЗИЛИ БИБЛИОТЕКУ
+int main (void) //блок основной программы
+{
+/*
+ Let's
+ go
+ to walk
+ */
+
+	printf ("Let's\n");
+	printf ("    go\n");
+	printf ("     to walk\n");
+	return 0;
+	}
---
+++ hw1/risunok.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //ПОДГРУЗИЛИ БИБЛИОТЕКУ
+int main (void) //блок основной программы
+{
+/*
+     *
+    ***
+   *****
+  *******
+   HHOHH
+   ZZZZZ
+ */
+
+	printf ("     *\n");
+	printf ("    ***\n");
+	printf ("   *****\n");
+	printf ("  *******\n");
+	printf ("   HHOHH\n");
+	printf ("   ZZZZZ\n");
+	return 0;
+	}
